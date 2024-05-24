8188212e462a5edad6084aa855d029ddd143fca0
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-24 14:04:35 +0200

    PDF  page
    Page for displaying the pdf of the party manifesto

---
+++ app/pdf/page.tsx
@@ -0,0 +1,17 @@
+import NavButton from "@/components/buttons/NavButton";
+import TestButton from "@/components/buttons/TestButton";
+import ManifestoComponent from "@/components/ManifestoComponent";
+
+export default function Home() {
+
+  
+    return (
+        <main className="flex flex-col items-center justify-between h-screen">
+            <div className="text-center place-items-center mb-3 w-full">
+                <h2 >Take a look at the manifesto and then click the button to get to the next page when you&apos;re done.</h2>
+            </div>
+            <ManifestoComponent></ManifestoComponent>
+        <NavButton href="/post-task"></NavButton>
+        </main>
+    );
+}
