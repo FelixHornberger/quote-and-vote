00fea4931cdaa657b7513c2214ec7334c199dcd2
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-24 14:01:20 +0200

    GetTaskPage API
    Generates a 0, 1 or 2. Depending on this, the href is set to Chat or PDF and the condition is set to active or passive.

---
+++ app/api/getTaskPages/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+type ResponseData = {
+  href: string;
+  condition: boolean;
+};
+
+export const GET = async (req: NextRequest) => {
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  let data: ResponseData;
+
+  if (randomNumber === 0) {
+    data = {
+      href: '/pdf',
+      condition: false,
+    };
+  } else if (randomNumber === 1) {
+    data = {
+      href: '/chat',
+      condition: false,
+    };
+  } else {
+    data = {
+      href: '/chat',
+      condition: true,
+    };
+  }
+
+  return NextResponse.json(data);
+};
+
+export const config = {
+  runtime: 'edge',
+};
