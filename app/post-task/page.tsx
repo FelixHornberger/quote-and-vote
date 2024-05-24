4256ed4ee672b4111f5aa7917e40bfb3fd43f63b
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-24 14:08:26 +0200

    Create page.tsx

---
+++ app/post-task/page.tsx
@@ -0,0 +1,17 @@
+import ArgumenTextarea from "@/components/ArgumentTextarea";
+import SubmitButton from "@/components/buttons/SubmitButton";
+import Likertscale from "@/components/Likertscale";
+
+export default function Home() {
+
+    return (
+        <main className="flex flex-col items-center justify-between text-center">
+            <h2 className="mb-3">Please state how likely you are to vote for this party</h2>
+            <Likertscale topic={'eligibilityAfter'} descriptionLeft="I would never vote for this party" descriptionRight={"I will vote for this party"} />
+            <p className="my-3">Please write down anything new that you have learned about the programme of the party.
+            </p>
+            <ArgumenTextarea/>
+            <SubmitButton />
+        </main>
+    );
+}
