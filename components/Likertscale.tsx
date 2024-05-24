c0a93e23188c2ed1f22fd32c499b6e8a9b337426
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-24 13:54:34 +0200

    Base Likertscale Component

---
+++ components/Likertscale.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useLikertscaleGradingStore } from '@/zustand/likertscale';
+import { ChangeEvent } from 'react';
+
+
+function Likertscale({topic, descriptionLeft, descriptionRight}: 
+    {topic: string, descriptionLeft: string, descriptionRight: string}) {
+
+  const setLikertscaleGrade = useLikertscaleGradingStore((state) => state.setLikertscaleGrade)
+
+  const handleRadioChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedValue = parseInt(e.target.value, 10);
+    setLikertscaleGrade({ [topic]: selectedValue });
+  };
+  return (
+    <div className='flex justify-center text-center w-full'>
+      <div className="inline-grid sm:flex sm:content-center sm:justify-center">
+        <p className="">{descriptionLeft}</p>
+        <ul className="list-none px-5 place-content-around place-items-center inline-grid sm:flex" id={topic}>
+          {[...Array(7).keys()].map((index) => (
+            <li key={index} className="px-2 scale-150">
+              <input
+                id={`${topic}-answer-${index + 1}-li`}
+                type="radio"
+                name={topic}
+                value={index - 3}
+                onChange={handleRadioChange}
+              />
+            </li>
+          ))}
+        </ul>
+        <p>{descriptionRight}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Likertscale;
