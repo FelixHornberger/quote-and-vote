ffdd3a0ddda5dd93a5ef0a2dabec5a8a7c6c571f
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-28 11:48:57 +0200

    Updated Description texts of the likertscales

--- app/post-task/page.tsx
+++ app/post-task/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <main className="flex flex-col items-center justify-between text-center">
             <h2 className="mb-3">Please state how likely you are to vote for this party</h2>
-            <Likertscale topic={'eligibilityAfter'} descriptionLeft="I would never vote for this party" descriptionRight={"I will vote for this party"} />
+            <Likertscale topic={'eligibilityAfter'} descriptionLeft="I would never vote for this party" descriptionRight={"I would vote for this party"} />
             <p className="my-3">Please write down anything new that you have learned about the programme of the party.
             </p>
             <ArgumenTextarea/>
--- components/QuestionElement.tsx
+++ components/QuestionElement.tsx
@@ -6,7 +6,7 @@ export default function QuestionElement({question, topic}:{question:string, topi
             <div className="flex text-center justify-center mb-3">
                 <h2>{question}</h2>
             </div>
-            <Likertscale topic={topic} descriptionLeft="Never would I vote for this party" descriptionRight={"I will vote for this party"} />
+            <Likertscale topic={topic} descriptionLeft="I would never vote for this party" descriptionRight={"I would vote for this party"} />
         </div>
     );
 }
