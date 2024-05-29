b57e32aa6cf1d45aea1c712aae4c5f989515306f
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-29 11:27:45 +0200

    Added Validation logic for SubmitButton and renamed it

--- components/buttons/SubmitButton.tsx
+++ components/buttons/SubmitButton.tsx
@@ -1,14 +1,27 @@
 'use client'
+import { useLikertscaleGradingStore } from '@/zustand/likertscale';
 import Link from 'next/link';
+import { useState } from 'react';
+import UserFeedback from '../UserFeedback';
 
-export default function NavButton(){
+export default function SubmitButton(){
 
-    // Submit logic has to be implemented
+    const [showUserFeedback, setVisbility] = useState(false);
+    const {likertscaleGrading} = useLikertscaleGradingStore();
+
+    const handleClick = (e: React.MouseEvent) => {
+        if (likertscaleGrading['eligibilityAfter'] !== '' ) { 
+        } else {
+          setVisbility(true);
+          e.preventDefault();
+        }
+    }
 
     return (
-        <div className='text-center flex justify-center'>
+        <div className='flex flex-col text-center justify-center'>
+            {showUserFeedback && <UserFeedback feedbackText='You cannot go to the next page without having stated all topics' />}
             <Link href="/end" passHref>
-                <button className='bg-custom-accent p-2 font-semibold my-3'>
+                <button className='bg-custom-accent p-2 font-semibold my-3' onClick={handleClick}>
                     Submit data
                 </button>
             </Link>
