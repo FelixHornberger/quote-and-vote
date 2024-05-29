4ace48b13883154a173286a41bec58ec9a79efad
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-29 11:33:05 +0200

    Changed Accent-color of likertscale to black

--- components/Likertscale.tsx
+++ components/Likertscale.tsx
@@ -13,11 +13,12 @@ function Likertscale({topic, descriptionLeft, descriptionRight}:
     const selectedValue = parseInt(e.target.value, 10);
     setLikertscaleGrade({ [topic]: selectedValue });
   };
+  
   return (
     <div className='flex justify-center text-center w-full'>
       <div className="inline-grid sm:flex sm:justify-center sm:items-center">
         <p className="flex-[30%]">{descriptionLeft}</p>
-        <ul className="list-none px-5 py-5 sm:py-0 place-content-around place-items-center inline-grid sm:flex " id={topic}>
+        <ul className="list-none px-5 py-5 sm:py-0 place-content-around place-items-center inline-grid sm:flex accent-black" id={topic}>
           {[...Array(7).keys()].map((index) => (
             <li key={index} className="px-2 scale-150">
               <input
