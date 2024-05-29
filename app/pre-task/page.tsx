cdac17dfc961c394c9b98894780f3a29d01c8d88
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-29 11:26:58 +0200

    Added new Questions to the PreTask page

--- app/pre-task/page.tsx
+++ app/pre-task/page.tsx
@@ -2,6 +2,7 @@ import NavButton from "@/components/buttons/NavButton";
 import PreTaskButton from "@/components/buttons/PreTaskButton";
 import QuestionElement from "@/components/QuestionElement";
 import QuestionElement2 from "@/components/QuestionElement2";
+import QuestionElement3 from "@/components/QuestionElement3";
 
 export default function Home() {
   
@@ -12,7 +13,8 @@ export default function Home() {
       </div>
       <div className="w-[75%] sm:w-[]">
         <QuestionElement2 question={"In what party are you interested?"}></QuestionElement2>
-        <QuestionElement question={"Would you vote for this party"} topic={"eligibilityBefore"}  ></QuestionElement>
+        <QuestionElement3 question="Would you vote for a national party in this party"></QuestionElement3>
+        <QuestionElement question={"How confident are you in this decision?"} topic={"eligibilityBefore"}  ></QuestionElement>
       </div>
       <PreTaskButton></PreTaskButton>
     </main>
