4e94ae0712d8f93cd2c6fb96410782297f406700
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-29 11:30:40 +0200

    Added PostTaskHeader for the post task page

--- app/post-task/page.tsx
+++ app/post-task/page.tsx
@@ -1,12 +1,13 @@
 import ArgumenTextarea from "@/components/ArgumentTextarea";
 import SubmitButton from "@/components/buttons/SubmitButton";
 import Likertscale from "@/components/Likertscale";
+import PostTaskHeader from "@/components/PostTaskHeader";
 
 export default function Home() {
 
     return (
         <main className="flex flex-col items-center justify-between text-center">
-            <h2 className="mb-3">Please state how likely you are to vote for this party</h2>
+            <PostTaskHeader></PostTaskHeader>
             <Likertscale topic={'eligibilityAfter'} descriptionLeft="I would never vote for this party" descriptionRight={"I would vote for this party"} />
             <p className="my-3">Please write down anything new that you have learned about the programme of the party.
             </p>
