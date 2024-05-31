64cc61318a83e42c11e341b6ea37f2d267e20bcd
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-31 14:03:19 +0200

    Update surveyID.ts

--- zustand/surveyID.ts
+++ zustand/surveyID.ts
@@ -3,10 +3,10 @@ import { create } from 'zustand'
 // Does this make senese? Not really but then i would have to figure out how i set input to only numeric values :() 
 interface IDtate {
     surveyID: string;
-    setsurveyID: (by: string) => void;
+    setSurveyID: (by: string) => void;
 }
 
-export const useIDtore = create<IDtate>()((set) =>({
+export const useIDStore = create<IDtate>()((set) =>({
     surveyID: "",
-    setsurveyID: (by) => set(({surveyID: by}))
+    setSurveyID: (by) => set(({surveyID: by}))
 }));
