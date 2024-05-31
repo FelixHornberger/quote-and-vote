cb9613683f87dce60fd4cffd8ec3ccdf75a4876d
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-05-31 11:50:58 +0200

    Changed Imports and formated post task page

--- app/post-task/page.tsx
+++ app/post-task/page.tsx
@@ -1,5 +1,5 @@
 import ArgumenTextarea from "@/components/ArgumentTextarea";
-import SubmitButton from "@/components/buttons/SubmitButton";
+import PostTaskButton from "@/components/buttons/PostTaskButton";
 import Likertscale from "@/components/Likertscale";
 import PostTaskHeader from "@/components/PostTaskHeader";
 
@@ -7,12 +7,16 @@ export default function Home() {
 
     return (
         <main className="flex flex-col items-center justify-between text-center">
-            <PostTaskHeader></PostTaskHeader>
-            <Likertscale topic={'eligibilityAfter'} descriptionLeft="I would never vote for this party" descriptionRight={"I would vote for this party"} />
-            <p className="my-3">Please write down anything new that you have learned about the programme of the party.
-            </p>
-            <ArgumenTextarea/>
-            <SubmitButton />
+            <div className="flex-coloum sm:justify-center sm:place-self-center text-center sm:w-[50%]">
+                <PostTaskHeader></PostTaskHeader>
+                <div className="text-left">
+                    <Likertscale topic={'eligibilityAfter'} descriptionLeft="less likly" descriptionRight={"more likely"} />
+                </div>
+                <p className="my-3 text-left">Please write down anything new that you have learned about the programme of the party.
+                </p>
+                <ArgumenTextarea/>
+                <PostTaskButton />
+            </div>
         </main>
     );
 }
