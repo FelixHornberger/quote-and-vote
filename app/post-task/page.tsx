20e3dc9e76789e215eead78e6b6a88d23c831124
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-06-02 23:39:40 +0200

    Styling post task update

--- app/post-task/page.tsx
+++ app/post-task/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                 <div className="flex flex-col border-2 border-neutral-900 p-5 mb-3">
                     <div className="text-left">
                         <Likertscale topic={'eligibilityAfter'} descriptionLeft="less likely" descriptionRight={"more likely"} />
-                        <p>If your opinion didn&apos;t change, click the button in the middle.</p>
+                        <p className="pt-2.5 sm:pt-0">If your opinion didn&apos;t change, click the button in the middle.</p>
                     </div>
                 </div>
                 <p className="mt-3.5 text-left">Write down anything new you have learnt about the party&apos;s manifesto.</p>
