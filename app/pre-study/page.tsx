963af0689a26670d14cf6f42fc716c14d7597ec3
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-06-02 16:23:33 +0200

    Added new answer Options to Pre-Study Page

--- app/pre-study/page.tsx
+++ app/pre-study/page.tsx
@@ -5,19 +5,19 @@ export default function Home() {
 
 
   const question1 = "How many members does the European Parliament have?";
-  const labels1 = ["a) 600","b) 705","c) 751","d) 800"];
+  const labels1 = ["a) 600","b) 705","c) 751","d) 800", "e) I don't know"];
 
   const question2 = "How often are European Parliament elections held?";
-  const labels2 = ["a) Every 3 years","b) Every 4 years","c) Every 5 years","d) Every 6 years"];
+  const labels2 = ["a) Every 3 years","b) Every 4 years","c) Every 5 years","d) Every 6 years", "e) I don't know"];
 
   const question3 = "Which of the following institutions shares legislative power with the European Parliament?";
-  const labels3 = ["a) The European Council","b) The European Commission","c) The Council of the European Union","d) The European Court of Justice"];
+  const labels3 = ["a) The European Council","b) The European Commission","c) The Council of the European Union","d) The European Court of Justice", "e) I don't know"];
 
   const question4 = "What is the main function of the European Parliament?";
-  const labels4 = ["a) To propose new EU laws","b) To pass and amend legislation","c) To manage the EU budget directly","d) To adjudicate legal disputes between member states"];
+  const labels4 = ["a) To propose new EU laws","b) To pass and amend legislation","c) To manage the EU budget directly","d) To adjudicate legal disputes between member states", "e) I don't know"];
 
   const question5 = "What is the process called when the European Parliament votes to approve or reject the European Commission as a whole?";
-  const labels5 = ["a) Confirmation hearing","b) Legislative review","c) Motion of censure","d) Investiture vote"];
+  const labels5 = ["a) Confirmation hearing","b) Legislative review","c) Motion of censure","d) Investiture vote", "e) I don't know"];
 
   return (
     <main className="flex flex-col items-center">
