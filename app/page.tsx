3d8efaaf2923f1696b895bfc20c2ad4f4edf6fc4
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-06-03 00:11:20 +0200

    Update page intro text

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,12 @@ export default function Home() {
         <div className="px-5 flex justify-center">
           <div className="task-box mb-3 max-w-prose place-items-center px-5 border border-neutral-900">
               <p className="">
-                You have been invited to take part in my online study. 
-                The European elections are on 9 June 2024 and we are electing national parties that can win seats in the European Parliament. 
-                Most national parties join together to form political groups. These groups have their own election programmes. 
-                The purpose of this study is to provide participants with different means to learn about a party of their choice and analyse how this impacts their engagement with the material and the amount of political knowledge gained.
-                The study will be conducted by Felix Hornberger and supervised by Dr. David Elsweiler from the University of Regensburg. 
-                The duration of the study is 15 minutes. Students at the University of Regensburg will receive 0.25 credit hours. 
-                The study can only be started once and should be completed in one go.
+                You are invited to participate in an online study conducted in the context of the upcoming European elections on 9 June 2024, where national parties will be elected to win seats in the European Parliament. 
+                These national parties often form political groups, each with its own election program. 
+                The purpose of this study is to provide participants with different means to learn about a party of their choice and analyse how this impacts their engagement with the material and the amount of political knowledge gained. 
+                The study is conducted by Felix Hornberger and supervised by Dr. David Elsweiler from the University of Regensburg. The study will take approximately 15 minutes to complete. 
+                University of Regensburg students will receive 0.25 credit hours for their participation. 
+                Please note that the study can only be started once and must be completed in a single session.
               </p>
           </div>
         </div>
